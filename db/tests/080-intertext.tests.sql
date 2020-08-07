fdade7ff7f0cc886e5f27406db3992a759317ef3
flow <flow@enceladus.orbit>
2020-08-07 22:28:36 +0200

    remove symlinks as they will not be installed by npm

--- db/tests/080-intertext.tests.sql
+++ db/tests/080-intertext.tests.sql
@@ -3,7 +3,7 @@
 -- \set ECHO queries
 
 /* ###################################################################################################### */
-\ir './_trm.sql'
+\ir '../_trm.sql'
 -- \ir './set-signal-color.sql'
 -- \ir './test-begin.sql'
 -- \pset pager on
